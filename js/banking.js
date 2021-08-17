bb9b30ee0217b8455c91820398a18574d7b3e1fd
zahidbinshad <you@example.com>
2021-08-17 16:50:44 +0600

    deposite and balance system updated. Need to work on validation later on

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,29 @@
 document.getElementById('btn-deposite').addEventListener('click', function () {
     //get the diposited amount from input 
     const dipositeInput = document.getElementById('diposite-amount');
-    const dipositeAmount = dipositeInput.value;
+    const newDipositeAmount = dipositeInput.value;
 
 
     let depositeTotal = document.getElementById('deposite-total');
-    depositeTotal.innerText = dipositeAmount;
+    const previousDepositeAmount = depositeTotal.innerText;
+    const newDepositeTotal = parseFloat(previousDepositeAmount) + parseFloat(newDipositeAmount);
+
+    depositeTotal.innerText = newDepositeTotal;
+
+    //update balnce 
+    const balanceTotal = document.getElementById('balance-total');
+
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const newBalanceAmount = parseFloat(previousBalanceTotal) + parseFloat(newDipositeAmount);
+
+    balanceTotal.innerText = newBalanceAmount;
+
+
+
+    //clear the input
+    dipositeInput.value = '';
 
 
 
